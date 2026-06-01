e77d0f9dc695101ea12f70d668e4460ff6189292
Neal Chandra <nel@mac.attlocal.net>
2026-05-31 17:53:11 -0700

    making transformer model

--- imdb_sentiment_analysis/data.py
+++ imdb_sentiment_analysis/data.py
@@ -19,3 +19,6 @@ X = (vectorizer.fit_transform(df['review_clean']))
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, list(df['label']), test_size=0.2, random_state=42)
+
+
+
